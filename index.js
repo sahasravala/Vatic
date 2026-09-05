2e621af43d6117d146f9077246c1bed2d918b3fc
Sahasra <sahasrav2116@gmail.com>
2026-09-05 16:59:33 -0400

    add express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Vatic is starting")
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Vatic API is running' })
+})
+
+app.listen(3000, () => {
+    console.log('Vatic server running on port 3000')
+})
